a770b6af9c63352e978518e75e1527692c347076
Beshoymoriis <128454109+Beshoymoriis@users.noreply.github.com>
2023-03-27 10:23:48 +0200

    create custom view body, appbar, search icon

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:note_app/widgets/custom_view_body.dart';
 
 class NoteView extends StatelessWidget {
   const NoteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: CustomViewBody(),
+    );
   }
 }
