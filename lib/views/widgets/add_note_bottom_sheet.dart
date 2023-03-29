c349fb066ce0b8195be56e3cd0339a061a420ade
Beshoymoriis <128454109+Beshoymoriis@users.noreply.github.com>
2023-03-29 11:19:39 +0200

    Create edit note view

---
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/custom_text_field.dart';
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            labelText: 'Title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            labelText: 'Content',
+            maxLines: 5,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:note_app/constans.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
+  const CustomTextField(
+      {super.key, required this.labelText, this.maxLines = 1});
+  final String labelText;
+  final int maxLines;
   @override
   Widget build(BuildContext context) {
     return TextField(
+      maxLines: maxLines,
       decoration: InputDecoration(
-        labelText: 'Title',
-        labelStyle: const TextStyle(color: kPrimaryColor, fontSize: 18),
+        labelText: labelText,
+        labelStyle: const TextStyle(color: kPrimaryColor, fontSize: 16),
         border: buildBorder(),
         enabledBorder: buildBorder(),
       ),
